b8d167fc98463581e05277ca506729fd6b052fcc
BayramKargin <bayram442814@gmail.com>
2024-03-13 15:41:22 +0300

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Pages/Home';
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
