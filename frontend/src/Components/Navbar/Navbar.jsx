38e9cb3c1e1f8567c9cb3ffecab5e0058bde68d1
BayramKargin <bayram442814@gmail.com>
2024-03-15 15:57:21 +0300

    Swap input added

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../Assest/logoblue.png'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+        <div className="lefth">
+            <img src={logo} alt="" />
+            <Link to="/"><div className="headerItem">Swap</div></Link>
+            <div className="headerItem">Tokens</div>
+        </div>
+        <div className="righth">
+            <div className="headerItem">Ethereum</div>
+            <div className="headerItem"><button>Connect</button></div>
+        </div>
+    </div>
   )
 }
 
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./CSS/Pages.css";
 
 const Home = () => {
-  return (
-    <div className='home'>Home</div>
-  )
-}
+  const [token1, setToken1] = useState("");
+  const [amount1, setAmount1] = useState("");
+  const [token2, setToken2] = useState("");
+  const [amount2, setAmount2] = useState("");
+  const [estimatedGas, setEstimatedGas] = useState("");
 
-export default Home
+  return (
+    <div className="home">
+      <div className="swap-container">
+      <h2 className="swap-header">Token Swap</h2> {/* Başlık eklendi */}
+        <div className="swap-box">
+          <div className="input-group">
+            <select value={token1} onChange={(e) => setToken1(e.target.value)}>
+              {/* Token listesi opsiyonları buraya ekleyin */}
+              <option value="">Select a Token</option>
+            </select>
+            <input
+              type="text"
+              value={amount1}
+              onChange={(e) => setAmount1(e.target.value)}
+              placeholder="amount"
+            />
+          </div>
+          <div className="input-group">
+            <select value={token2} onChange={(e) => setToken2(e.target.value)}>
+              {/* Token listesi opsiyonları buraya ekleyin */}
+              <option value="">Select a Token</option>
+            </select>
+            <input
+              type="text"
+              value={amount2}
+              onChange={(e) => setAmount2(e.target.value)}
+              placeholder="amount"
+            />
+          </div>
+          <div className="estimated-gas">
+            <label>Estimated Gas: {estimatedGas}</label>
+          </div>
+          <button
+            className="swap-button"
+            onClick={() => {
+              /* Swap fonksiyonu çağrısı */
+            }}
+          >
+            Swap
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
