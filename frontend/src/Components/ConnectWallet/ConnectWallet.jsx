2c16a0f625e1965c1ecc908a3c211a7bcb9c3749
Burak Kozluca <burak_kozluca@hotmail.com>
2024-05-07 15:48:52 +0300

    show wallet balance

--- frontend/src/Components/ConnectWallet/ConnectWallet.jsx
+++ frontend/src/Components/ConnectWallet/ConnectWallet.jsx
@@ -1,18 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ethers } from 'ethers';
 
 const ConnectWallet = () => {
     const [account, setAccount] = useState(null);
+    const [balance, setBalance] = useState(null);
+
+    // Ethers Provider'ı kullanarak bağlantıyı ve bakiye sorgulamayı yöneten fonksiyon
+    const getBalance = async (provider, address) => {
+        const balance = await provider.getBalance(address);
+        const formattedBalance = ethers.utils.formatEther(balance);
+        setBalance(formattedBalance);
+    };
 
     // Cüzdan bağlantısını başlatma fonksiyonu
     const connectWalletHandler = async () => {
-        // window.ethereum nesnesine erişim sağlama
         if (window.ethereum && window.ethereum.isMetaMask) {
             try {
-                // MetaMask ile etkileşimde bulunma ve hesapları alma
                 const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-                // İlk hesabı seçme
+                const provider = new ethers.providers.Web3Provider(window.ethereum);
                 setAccount(accounts[0]);
+                await getBalance(provider, accounts[0]);
             } catch (error) {
                 console.error("Error connecting to MetaMask", error);
             }
@@ -24,7 +31,10 @@ const ConnectWallet = () => {
     return (
         <div>
             {account ? (
-                <p>Connected as: {account}</p>
+                <div>
+                    <p>Connected as: {account}</p>
+                    <p>Balance: {balance} ETH</p>
+                </div>
             ) : (
                 <button onClick={connectWalletHandler}>Connect</button>
             )}
