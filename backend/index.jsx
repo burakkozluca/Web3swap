4c25ab50ac9a4b2a6375e6424f105147746766a2
BayramKargin <bayram442814@gmail.com>
2024-05-21 12:22:30 +0300

    navbar and chat

--- backend/index.jsx
+++ backend/index.jsx
@@ -3,11 +3,52 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const Binance = require("node-binance-api");
+const fetch = require('node-fetch');
+const http = require('http');
+const socketIo = require('socket.io');
 
-const api_key =
-  "s7DSYB2VHOsxz24ZiR9pMmlxzwJIjMgY225ypc31b1bcnRGBEvStWsDSm20Kmuev";
-const api_secret =
-  "OMGnXrLvXNmgt8zjOf1rgRtHWQHLkckRQL8BZPDqBhNvzOtMjNfbtzsD8TIxGirL";
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('Yeni bir kullanıcı bağlandı:', socket.id);
+
+  // Kullanıcı mesaj gönderdiğinde bu mesajı tüm kullanıcılara ilet
+  socket.on('message', (message) => {
+    console.log('Mesaj alındı:', message);
+    socket.broadcast.emit('message', message) // Tüm bağlı kullanıcılara mesajı gönder
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Kullanıcı ayrıldı:', socket.id);
+  });
+});
+
+const api_key = "s7DSYB2VHOsxz24ZiR9pMmlxzwJIjMgY225ypc31b1bcnRGBEvStWsDSm20Kmuev";
+const api_secret = "OMGnXrLvXNmgt8zjOf1rgRtHWQHLkckRQL8BZPDqBhNvzOtMjNfbtzsD8TIxGirL";
+
+const api_key_coingecko = "CG-EuXL6u3vtLzpKN7yEVzv6cb2"
+
+const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd';
+const options = {
+  method: 'GET',
+  headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-EuXL6u3vtLzpKN7yEVzv6cb2'}
+};
+
+fetch(url, options)
+  .then(res => res.json())
+  .then(data => {
+    // Kripto para birimlerinin her biri için name, symbol ve current_price bilgilerini yazdır
+    data.forEach(coin => {
+      console.log(`Name: ${coin.name}, Symbol: ${coin.symbol}, Price: ${coin.current_price} USD`);
+    });
+  })
+  .catch(err => console.error('error:' + err));
 
 const binance = new Binance().options({
   APIKEY: api_key,
@@ -50,29 +91,7 @@ app.get("/coins", async (req, res) => {
   });
 });
 
-// binance.prices((error, ticker) => {
-//   if (error) {
-//     console.error(error);
-//   } else {
-//     const symbols = Object.keys(ticker);
-//     console.log('Tüm semboller:', symbols);
-//   }
-// });
-
-// const fetchCoin = async (req,res) =>{
-
-//   binance.prices((error, ticker) => {
-//     if (error) {
-//       console.error(error);
-//     } else {
-//       const symbols = Object.keys(ticker);
-//       res.send(symbols)
-//       console.log('Tüm semboller:', symbols);
-//     }
-//   });
-// }
-
-app.listen(port, (error) => {
+server.listen(port, (error) => {
   if (!error) {
     console.log("Server Running on Port " + port);
   } else {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Pages/Home';
+import Chat from './Pages/Chat';
+
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +13,7 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/chat' element={<Chat/>} />
       </Routes>
       </BrowserRouter>
     </div>
